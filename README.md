c072a44b37234b7a05f01df1bbc73ab5249c1c80
Alexandre Dumont <adumont@gmail.com>
2017-02-11 10:50:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+# Bitcoin To Twitter
+
+This is a simple bot that will tweet the price of a bitcoin to a twitter account. See app.json to know the config variables.
+
+## Setup
+
+Deploy using the Deploy button
---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import random
+import re
+import sys
+import twitter
+import requests
+
+# Load Heroku Config Variables (https://devcenter.heroku.com/articles/config-vars)
+MY_CONSUMER_KEY = os.environ['MY_CONSUMER_KEY']
+MY_CONSUMER_SECRET = os.environ['MY_CONSUMER_SECRET']
+MY_ACCESS_TOKEN_KEY = os.environ['MY_ACCESS_TOKEN_KEY']
+MY_ACCESS_TOKEN_SECRET = os.environ['MY_ACCESS_TOKEN_SECRET']
+TWEET_ACCOUNT = os.environ['TWEET_ACCOUNT']
+
+ODDS = int(os.getenv('ODDS', 8))
+DEBUG = os.getenv('DEBUG', False) == "True"
+
+# some API Parameters for user_timeline
+INCLUDE_RTS = os.getenv('INCLUDE_RTS', True) == "True"
+EXCLUDE_REPLIES = os.getenv('EXCLUDE_REPLIES', False) == "True"
+
+# build a (connected) Twitter API object
+def connect():
+    api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
+                          consumer_secret=MY_CONSUMER_SECRET,
+                          access_token_key=MY_ACCESS_TOKEN_KEY,
+                          access_token_secret=MY_ACCESS_TOKEN_SECRET)
+    return api
+
+# post tweet to twitter (or console)
+def post_tweet(api, debug, ebook_tweet):
+    if debug == True:
+        print "DEBUG: " + ebook_tweet
+    else:
+        status = api.PostUpdate(ebook_tweet)
+        print status.text.encode('utf-8')        
+
+def get_bitcoin_price():
+   a = requests.get('https://api.coindesk.com/v1/bpi/currentprice/EUR.json').json()
+   return a['bpi']['EUR']['rate'].replace('.', ',')
+    
+def main(argv):
+    debug = DEBUG
+    
+    # determine odds of running
+    if ODDS>0 and debug==False:
+        guess = random.choice(range(ODDS))
+    else:
+        guess = 0
+
+    if guess > 0:
+        print str(guess) + " No, sorry, not this time."
+        sys.exit()
+
+    # connect to API if necessary
+    if debug == False:
+        api = connect()
+    else:
+        api = None
+
+    tweet = "El precio de 1 bitcoin es " + get_bitcoin_price() + " EUR" 
+        
+    post_tweet(api, debug, tweet)
+
+            
+if __name__ == "__main__":
+    main(sys.argv[1:])
